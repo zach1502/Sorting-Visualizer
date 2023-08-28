3621e236ed8064c2708796795237012a6888ec3b
Zachary Chan <zca121@sfu.ca>
2023-08-27 19:53:25 -0700

    Extract constants and descriptions

---
+++ src/utils/AlgorithmDescriptions.js
@@ -0,0 +1,277 @@
+
+const algorithmDescriptions = {
+  'Bubble Sort': {
+    paragraphs: [
+      'Bubble Sort is one of the simplest sorting algorithms and it works by repeatedly stepping through the list of items (like numbers) and comparing each pair of adjacent items and swapping them if they are in the wrong order. The pass through the list is repeated until no swaps are needed, which indicates that the list is sorted. The algorithm gets its name from the way elements "bubble" to the top of the list.',
+      'One important thing to note is that Bubble Sort is not very efficient for lists with a large number of items. Its time complexity is O(n^2) in the worst and average case, and O(n) in the best case (when the list is already sorted). For this reason, it is not often used in practice, but it is a good starting point for understanding the principles of sorting.',
+      'Bubble Sort is a stable algorithm, which means that it preserves the order of items with equal keys. It is also an in-place algorithm, which means that it does not require any extra memory space to sort the list.',
+    ],
+  },
+
+  'Quick Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Merge Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Insertion Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Selection Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Cocktail Shaker Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Radix Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Intro Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Heap Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Block Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Shell Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Tim Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Exchange Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Cycle Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Patience Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Smooth Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Strand Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Comb Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Gnome Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Odd-Even Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Pigeonhole Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Bucket Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Counting Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Spread Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Pancake Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'I Can\'t Believe It Can Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Bitonic Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Bogo Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Stooge Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Slow Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Stalin Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Sleep Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Delete Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+
+  'Tree Sort': {
+    paragraphs: [
+      'text1',
+      'text2',
+      'text3',
+    ],
+  },
+}
+
+
+export default algorithmDescriptions;
