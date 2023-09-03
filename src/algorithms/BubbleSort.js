567a84dc54057a64b68bb89532dcbc99ff39c02d
Zachary Chan <zca121@sfu.ca>
2023-09-03 12:41:10 -0700

    fix bbl sort short circuit

--- src/algorithms/BubbleSort.js
+++ src/algorithms/BubbleSort.js
@@ -4,9 +4,9 @@ function* bubbleSort(arr) {
   yield _.cloneDeep(arr);
   let n = arr.length;
 
-  let swapped = false;
-
   for (let i = 0; i < n - 1; i++) {
+    let swapped = false;
+
     for (let j = 0; j < n - i - 1; j++) {
       arr[j].isComparing = true;
       arr[j + 1].isComparing = true;
